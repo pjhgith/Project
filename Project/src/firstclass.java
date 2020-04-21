b5313fb9a96882390f389814bb2d223e02cb17f9
calappidae <62378525+calappidae@users.noreply.github.com>
2020-04-21 09:25:41 +0800

    Update firstclass.java

--- Project/src/firstclass.java
+++ Project/src/firstclass.java
@@ -2,5 +2,6 @@ public class firstclass {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("by calappidae");
+        System.out.println("edit by 1");
     }
 }
